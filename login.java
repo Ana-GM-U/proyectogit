6fb1a1916f9f7bc0a4a8ddc957475541287a2d86
AnaGMU <anagutierrezmoncadaa@gmail.com>
2024-10-10 17:17:27 -0300

    loginv2

--- login.java
+++ login.java
@@ -1,6 +1,6 @@
 public class login {
     public static void main(String[] args) {
-        System.out.println("edrgf2");
+        System.out.println("edrgf2    ver2");
         //dncfj
         //nfbkm
     }
